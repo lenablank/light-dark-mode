4769557a033e2a572ddfe8f44a285fb6ccbdc062
Lena Blank <mine.blank@gmail.com>
2021-04-09 15:33:47 +0200

    add local storage

--- script.js
+++ script.js
@@ -37,11 +37,24 @@ function switchTheme(event) {
     // console.log(event.target.checked);
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 // event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
