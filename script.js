fb1db506df71f7e26db068d84e228c9c39a4fa46
Lena Blank <mine.blank@gmail.com>
2021-04-09 14:19:00 +0200

    selecting all elements

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image2');
+const textBox = document.getElementById('text-box');
+
 
 // switch theme dynamically
 function switchTheme(event) {
