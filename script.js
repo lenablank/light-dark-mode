7356c0e6334f78025aa4aafe229f5305dc237521
Lena Blank <mine.blank@gmail.com>
2021-04-09 14:51:26 +0200

    dark mode styles

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image2');
 const textBox = document.getElementById('text-box');
 
+// dark mode styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    // console.log(toggleIcon.children);
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    image1.src = 'img/undraw_Nature_fun_re_dark.svg';
+    image2.src = 'img/undraw_adventure_map_dark.svg';
+}
 
 // switch theme dynamically
 function switchTheme(event) {
     // console.log(event.target.checked);
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 // event listener
